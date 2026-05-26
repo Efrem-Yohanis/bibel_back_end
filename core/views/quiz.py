95d066bc0bc71b55a95204eeef984318688dd053
Bin <126686114+Efrem-Yohanis@users.noreply.github.com>
2026-05-26 18:23:21 +0300

    Update quiz.py

--- core/views/quiz.py
+++ core/views/quiz.py
@@ -5,7 +5,7 @@ Quiz Views - Quiz management endpoints
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny  # Add AllowAny here
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
