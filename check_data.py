cb942e7856aa955c7620e81cd6aa35a1816ee3dd
Efrem-Yohanis <efremyohanis116@gmail.com>
2026-06-01 00:27:14 -0700

    test

--- check_data.py
+++ check_data.py
@@ -2,8 +2,8 @@ import requests
 
 url = "https://bibel-quiz.onrender.com/api/auth/register"
 payload = {
-    "username": "efrem_test_user_07",
-    "email": "jtechethiopia@gmail.com",
+    "username": "efrem_test_user_01",
+    "email": "ephadaniel177@gmail.com",
     "password": "StrongPass123!",
     "confirm_password": "StrongPass123!"
 }
