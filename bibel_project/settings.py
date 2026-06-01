00deb15ca1170f85c8acc15bb270a2cc7c41cef5
Efrem-Yohanis <efremyohanis116@gmail.com>
2026-05-31 23:36:54 -0700

    Support SMTP_* env vars for email backend

--- bibel_project/settings.py
+++ bibel_project/settings.py
@@ -211,17 +211,16 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Email Configuration - Get from Render environment variables
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' if all([
-    os.environ.get('EMAIL_HOST_USER'),
-    os.environ.get('EMAIL_HOST_PASSWORD')
-]) else 'django.core.mail.backends.console.EmailBackend'
-
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_HOST = os.environ.get('EMAIL_HOST', os.environ.get('SMTP_HOST', 'smtp.gmail.com'))
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', os.environ.get('SMTP_PORT', 587)))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', os.environ.get('SMTP_USERNAME', ''))
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', os.environ.get('SMTP_PASSWORD', ''))
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', os.environ.get('SMTP_FROM_EMAIL', EMAIL_HOST_USER))
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' if all([
+    EMAIL_HOST_USER,
+    EMAIL_HOST_PASSWORD
+]) else 'django.core.mail.backends.console.EmailBackend'
 
 ROOT_URLCONF = 'bibel_project.urls'
 
